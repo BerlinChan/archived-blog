e66124cff995431a1b6bfb9b8ea499a8b96610f5
Berlin Chan <berlinchancom@gmail.com>
2019-08-26 11:26:40 +0800

    移动post中script到js文件

---
+++ wp-content/uploads/2016/01/krpano-tour/index.js
@@ -0,0 +1,8 @@
+embedpano({
+  swf: '//archived-blog.berlinchan.com/wp-content/uploads/2016/01/krpano-tour/tour.swf',
+  xml: '//archived-blog.berlinchan.com/wp-content/uploads/2016/01/krpano-tour/tour.xml',
+  target: 'krpano',
+  html5: 'prefer',
+  passQueryParameters: true,
+  bgcolor: '#ffffff',
+})
